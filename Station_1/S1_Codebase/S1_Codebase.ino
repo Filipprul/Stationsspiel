38688465514a9dd438179e0a2ea746b8e6a187c6
Filipp <filipprul2007@gmail.com>
2026-01-15 18:28:58 +0100

    Add MQTT and WiFi support to Station 1 codebase
    Integrated WiFi and MQTT functionality using the PubSubClient library for communication. Refactored pin definitions and logic to support sender and receiver pins, and added MQTT-based status reporting when the puzzle is solved. Also included functions for WiFi setup, MQTT connection management, and message handling. Renamed and reorganized Station 0 code file for better structure.

--- Station_1/S1_Codebase/S1_Codebase.ino
+++ Station_1/S1_Codebase/S1_Codebase.ino
@@ -1,32 +1,119 @@
-const int kabelPins[] = {1, 2, 3, 4};
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "your_SSID"; // Ans WLAN anspassen
+const char* password = "your_PASSWORD"; // Ans WLAN anspassen
+const char* mqtt_server = "your_IP"; // Lokale IP-Adresse
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+int value = 0;
+
+const int senderPins[] = {0, 1, 2, 3};
+const int empfangPins[] = {4, 5, 6, 7};
 bool status = false;
+
+const int contPin = 8;
 
 void setup() {
   Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
-  for(i = 0; i < 4; i++){
-    pinMode(kabelPins[i], INPUT_PULLUP);
+  for(int i = 0; i < 4; i++){
+    pinMode(senderPins[i], OUTPUT);
+    digitalWrite(senderPins[i], LOW);
+    pinMode(empfangPins[i], INPUT_PULLDOWN);
   }
   Serial.println("System bereit!");
+}
+
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Verbindet mit WiFi: ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print("...");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi verbunden");
+    Serial.println("IP addresse: ");
+    Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+    Serial.print("Nachricht angekommen. Topic: ");
+    Serial.print(topic);
+    Serial.print(". Nachricht: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++) {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
+
+    if (String(topic) == "SF/Sub") {
+        Serial.print("Ändert status zu: ");
+        if (messageTemp == "on") {
+            Serial.println("An");
+            digitalWrite(contPin, HIGH);
+        } else if (messageTemp == "off") {
+            Serial.println("Aus");
+            digitalWrite(contPin, LOW);
+        }
+    }
+}
+
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Versuche MQTT Verbindung...");
+        String clientId = "ESP32-" + WiFi.macAddress();
+        if (client.connect(clientId.c_str())) {
+            Serial.println("Verbunden!");
+            client.subscribe("SF/Sub"); // MQTT-Kommunikation
+        } else {
+            Serial.print("Fehler, rc=");
+            Serial.print(client.state());
+            Serial.println(" Versuche es in 5 Sekunden erneut.");
+            delay(5000);
+        }
+    }
 }
 
 void check(){
   if (status) return;
 
-  bool pinsdrin = false;
+  int korrektePins = 0;
   for(int i = 0; i < 4; i++){
-    if (digitalRead(kabelPins[i]) == HIGH){
-      pinsdrin = true;
+    digitalWrite(senderPins[i], HIGH);
+    delay(5);
+    if (digitalRead(empfangPins[i]) == HIGH){
+      korrektePins++;
     }
+    digitalWrite(senderPins[i], LOW);
   }
 
-  if (pinsdrin == false){
+  if (korrektePins == 4){
     status = true;
-    Serial.println("Rätsel gelöst!")
+    client.publish("SF/station1/status", "geloest"); // MQTT-Nachricht
+    Serial.println("Rätsel gelöst!");
   }
 }
 
 void loop(){
+  if (!client.connected()) {
+      reconnect();
+  }
+  client.loop();
   check();
   delay(100);
 }
